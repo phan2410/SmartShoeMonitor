e4a8752c2137a3975c5c294af7ec85855490016e
phan2410 <phan2410@gmail.com>
2018-06-03 21:05:26 +0700

    added SmartShoeFirmware

---
+++ misc/ArduinoForSmartShoe/ArduinoForSmartShoe.ino
@@ -0,0 +1,119 @@
+volatile uint16_t instantRead = 0;
+volatile uint16_t tmpUInt = 0;
+byte pendingBytes[17] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,13};
+volatile uint8_t currentAnalogChannel = 0;
+volatile uint8_t tmpUShort = 0;
+bool LedState = HIGH;
+uint16_t LedOnMSec = 0;
+uint16_t LedOffMSec = 0;
+int incomingByte;
+int stateCode;
+bool isBeingCommanded = false;
+uint8_t NumOfCommandBytes = 0;
+
+void setup() {   
+  cli();//stop interrupts  
+  //Initialize ADC 
+  ADMUX = 0;
+  ADCSRA = 0b10001111;//ADEN|ADIE|XTAL/128
+  //Initialize TMR1
+  TCCR1A &= 0b00001100;
+  TCCR1B &= 0b00101000;
+  TCCR1B |= 0b00001000;
+  TIMSK1 &= 0b11011010;
+  TIMSK1 |= 0b00000010;
+  OCR1A = 19999;
+  TCNT1 = 0;
+  //Initialize TMR2
+  TCCR2A &= 0b00001110;
+  TCCR2A |= 0b00000010;
+  TCCR2B &= 0b00110000;
+  TIMSK2 &= 0b11111010;
+  TIMSK2 |= 0b00000010;
+  OCR2A = 71;
+  TCNT2 = 0;
+  sei();//enable interrupts
+  Serial.begin(250000);
+  while (!Serial);//waiting for serial initialization
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+ISR(TIMER1_COMPA_vect) {  
+  TCCR2B |= 0b00000011;//start TMR2
+}
+
+ISR(TIMER2_COMPA_vect) {  
+  ADMUX = (ADMUX & 0xF8)|currentAnalogChannel;
+  ADCSRA |= 0b01000000;//start ADC conversion
+  if (++currentAnalogChannel == 8) {
+    TCCR2B &= 0x11111000;//stop TMR2
+    TCNT2 = 0;
+  }
+}
+
+ISR(ADC_vect) {
+  instantRead = ADCL | (ADCH << 8);
+  tmpUInt = instantRead/43;
+  pendingBytes[tmpUShort] = byte(tmpUInt + 48);
+  pendingBytes[++tmpUShort] = byte(instantRead - tmpUInt*43 + 48);
+  ++tmpUShort;
+  if (currentAnalogChannel == 8) {
+    Serial.write(pendingBytes,17);
+    currentAnalogChannel = 0;
+    tmpUShort = 0;    
+  }
+}
+
+void startTMR1() {
+  TCCR1B |= 0b00000010;
+}
+
+void stopTMR1() {
+  TCCR1B &= 0b11111000;
+  TCNT1 = 0;
+}
+
+void loop() {
+  if (Serial.available() != 0) {
+    incomingByte = Serial.read();
+    if (isBeingCommanded) {
+      ++NumOfCommandBytes;
+    }
+    if (incomingByte == 54) {
+      isBeingCommanded = true;
+      NumOfCommandBytes = 1;      
+    } else if (incomingByte == 99) {
+      if (NumOfCommandBytes == 3) {
+        if (stateCode == 9) {
+          stopTMR1();
+          LedOnMSec = 9;
+          LedOffMSec = 567;
+        } else if (stateCode == 18) {
+          stopTMR1();
+          LedOnMSec = 180;
+          LedOffMSec = 180;
+        } else if (stateCode == 27) {
+          startTMR1();
+          LedOnMSec = 45;
+          LedOffMSec = 45;        
+        } else if (stateCode == 36) {
+          stopTMR1();
+          LedOnMSec = 0;
+          LedOffMSec = 0;          
+        } 
+      }
+      isBeingCommanded = false;
+    } else {
+      stateCode = incomingByte;
+    }
+  } 
+  if (LedOnMSec) {
+    LedState = !LedState;
+    digitalWrite(LED_BUILTIN, LedState);
+    if (LedState) {
+      delay(LedOnMSec);
+    } else {
+      delay(LedOffMSec);
+    }
+  }
+}
---
+++ misc/Esp8266ForSmartShoe/Esp8266ForSmartShoe.ino
@@ -0,0 +1,74 @@
+#include <ESP8266WiFi.h>
+#define _DebugMode_ 0
+
+//wifi configuration
+const char* ssid = "dlink1";
+const char* password = "123456aa";
+const char* ServerIP = "192.168.0.112";
+const int listeningPortOfServer = 11247;
+
+WiFiClient client;
+bool isArduinoWorking = false;
+
+void setup() {
+  Serial.begin(250000);
+  while (!Serial);//waiting for serial initialization
+  delay(100);//waiting for the arduino to keep up
+#if _DebugMode_
+  Serial.println("\n\n\n-------------------------------");
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+#else
+  commandArduino(9);
+#endif
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+#if _DebugMode_
+    Serial.print(".");
+#endif
+  }
+#if _DebugMode_
+  Serial.print("\nWiFi Connected With IP Address:");
+  Serial.println(WiFi.localIP());
+  Serial.print("Try communicating with ");
+  Serial.println(ServerIP);
+#else
+  commandArduino(18);
+#endif
+  while (!client.connect(ServerIP, listeningPortOfServer)) {
+    delay(500);
+#if _DebugMode_
+    Serial.print(".");
+#endif    
+  }
+#if _DebugMode_
+  Serial.println("Welcome To Smart Shoe Server !");
+  Serial.flush();  
+#else
+  commandArduino(27);
+  isArduinoWorking = true;
+#endif
+}
+
+void loop() {  
+  client.print('R' + Serial.readStringUntil('\r'));
+  if (!client.connected()) {
+    if (isArduinoWorking) {
+  #if !_DebugMode_
+      commandArduino(36); 
+  #endif    
+      Serial.flush();
+      client.stop();
+      isArduinoWorking = false;
+      ESP.reset();
+    }
+  }
+}
+
+void commandArduino(uint8_t commandCode) {
+  Serial.write(54);
+  Serial.write(commandCode);
+  Serial.write(99);
+}
+
