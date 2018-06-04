d4a0a159d3b0199f185a23e2da7b637496a36b4d
phan2410 <phan2410@gmail.com>
2018-06-04 14:57:02 +0700

    free running adc mode on arduino

--- misc/ArduinoForSmartShoe/ArduinoForSmartShoe.ino
+++ misc/ArduinoForSmartShoe/ArduinoForSmartShoe.ino
@@ -15,7 +15,9 @@ void setup() {
   cli();//stop interrupts  
   //Initialize ADC 
   ADMUX = 0;
-  ADCSRA = 0b10001111;//ADEN|ADIE|XTAL/128
+  ADCSRB &= 0b11111000;//ADC Free Running Mode
+  ADCSRA &= 0xFC;
+  ADCSRA |= 0b10000100;//ADEN|XTAL/16
   //Initialize TMR1
   TCCR1A &= 0b00001100;
   TCCR1B &= 0b00101000;
@@ -24,44 +26,35 @@ void setup() {
   TIMSK1 |= 0b00000010;
   OCR1A = 19999;
   TCNT1 = 0;
-  //Initialize TMR2
-  TCCR2A &= 0b00001110;
-  TCCR2A |= 0b00000010;
-  TCCR2B &= 0b00110000;
-  TIMSK2 &= 0b11111010;
-  TIMSK2 |= 0b00000010;
-  OCR2A = 71;
-  TCNT2 = 0;
   sei();//enable interrupts
   Serial.begin(250000);
   while (!Serial);//waiting for serial initialization
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+  startTMR1();
 }
 
 ISR(TIMER1_COMPA_vect) {  
-  TCCR2B |= 0b00000011;//start TMR2
-}
-
-ISR(TIMER2_COMPA_vect) {  
-  ADMUX = (ADMUX & 0xF8)|currentAnalogChannel;
-  ADCSRA |= 0b01000000;//start ADC conversion
-  if (++currentAnalogChannel == 8) {
-    TCCR2B &= 0x11111000;//stop TMR2
-    TCNT2 = 0;
-  }
+  ADCSRA |= 0b01111000;//ADSC|ADATE|ADIF|ADIE
 }
 
 ISR(ADC_vect) {
+  digitalWrite(10,HIGH);
   instantRead = ADCL | (ADCH << 8);
+  ADMUX = (ADMUX & 0xF8)|((++currentAnalogChannel) & 0x07);
   tmpUInt = instantRead/43;
   pendingBytes[tmpUShort] = byte(tmpUInt + 48);
   pendingBytes[++tmpUShort] = byte(instantRead - tmpUInt*43 + 48);
   ++tmpUShort;
   if (currentAnalogChannel == 8) {
+    ADCSRA &= 0b10010111;//ADSC|ADATE|ADIE
     Serial.write(pendingBytes,17);
     currentAnalogChannel = 0;
-    tmpUShort = 0;    
-  }
+    tmpUShort = 0; 
+  }  
+  digitalWrite(10,LOW);
 }
 
 void startTMR1() {
@@ -74,46 +67,5 @@ void stopTMR1() {
 }
 
 void loop() {
-  if (Serial.available() != 0) {
-    incomingByte = Serial.read();
-    if (isBeingCommanded) {
-      ++NumOfCommandBytes;
-    }
-    if (incomingByte == 54) {
-      isBeingCommanded = true;
-      NumOfCommandBytes = 1;      
-    } else if (incomingByte == 99) {
-      if (NumOfCommandBytes == 3) {
-        if (stateCode == 9) {
-          stopTMR1();
-          LedOnMSec = 9;
-          LedOffMSec = 567;
-        } else if (stateCode == 18) {
-          stopTMR1();
-          LedOnMSec = 180;
-          LedOffMSec = 180;
-        } else if (stateCode == 27) {
-          startTMR1();
-          LedOnMSec = 45;
-          LedOffMSec = 45;        
-        } else if (stateCode == 36) {
-          stopTMR1();
-          LedOnMSec = 0;
-          LedOffMSec = 0;          
-        } 
-      }
-      isBeingCommanded = false;
-    } else {
-      stateCode = incomingByte;
-    }
-  } 
-  if (LedOnMSec) {
-    LedState = !LedState;
-    digitalWrite(LED_BUILTIN, LedState);
-    if (LedState) {
-      delay(LedOnMSec);
-    } else {
-      delay(LedOffMSec);
-    }
-  }
+
 }
